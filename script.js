f4b6b4097c74003984f9dcea69fb9ed49fb66a5a
Lauriene <laurienea840@gmail.com>
2024-12-03 10:00:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-let filaDeEspera = [];
+let baralho = []
 
-filaDeEspera.push("Matheus");
-console.log("Matheus entrou na fila.");
+while (true) {
+    let menu = `Baralho de cartas:
+Quantidade de cartas: ${baralho.length}
+Escolha uma opção:
+1. Adicionar uma carta
+2. Puxar uma carta
+3. Sair`
 
-filaDeEspera.push("Marcos");
-console.log("Marcos entrou na fila.");
+    let escolha = prompt(menu)
 
-filaDeEspera.push("Ana");
-console.log("Ana entrou na fila.");
+    if (escolha === "1") {
+        let nomeCarta = prompt("Digite o nome da carta:")
 
-if (filaDeEspera.length === 0) {
-    console.log("Fila está vazia.");
-} else {
-    let consultado = filaDeEspera.shift();
-    console.log(`${consultado} saiu da fila.`);
+        if (nomeCarta && nomeCarta.trim() !== "") {
+            baralho.push(nomeCarta)
+            alert(`${nomeCarta} foi adicionada ao topo do baralho.`)
+        } else {
+            alert("Nome da carta inválido! Tente novamente.")
+        }
+    } else if (escolha === "2") {
+        if (baralho.length === 0) {
+            alert("O baralho está vazio.")
+        } else {
+            let cartaPuxada = baralho.pop()
+            alert(`Você puxou a carta: ${cartaPuxada}`)
+        }
+    } else if (escolha === "3") {
+        alert("Encerrando programa. Até mais!")
+        break
+    } else {
+        alert("Opção inválida! Tente novamente.")
+    }
 }
-
-if (filaDeEspera.length === 0) {
-    console.log("Fila está vazia.");
-} else {
-    let consultado = filaDeEspera.shift();
-    console.log(`${consultado} saiu da fila.`);
-}
-
-if (filaDeEspera.length === 0) {
-    console.log("Fila está vazia.");
-} else {
-    let consultado = filaDeEspera.shift();
-    // console.log(`${consultado} saiu da fila.`);
-}
-
-if (filaDeEspera.length === 0) {
-    console.log("Fila está vazia.");
-} else {
-    let consultado = filaDeEspera.shift();
-    console.log(`${consultado} saiu da fila.`)}
